d6c1211ffab851a032560e096f8f6ba8776d08e2
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2020-10-27 00:50:44 -0500

    created static method reverse in ReverseArray Class

--- Java_Algo/ReverseArray.java
+++ Java_Algo/ReverseArray.java
@@ -1,6 +1,25 @@
 package Java_Algo; 
 
+import java.util.Arrays;
+
+// 1st case even number of values 
+// 0, 1, 2, 3
+// 3, 1, 2, 0
+// 3 2 1 0
+
+// second case odd number of values 
+// 0, 1, 2, 3, 4
+// 4, 1, 2, 3, 0
+// 4, 3, 2, 1, 0
 public class ReverseArray {
+    static int[] reverse(int[] array) {
+        for(int i =0; i < Math.floor(array.length/2); i++) {
+            int temp = array[i]; 
+            array[i] = array[array.length - 1 - i]; 
+            array[array.length - 1 - i] = temp; 
+        }
+        return array; 
+    }
     public static void main(String[] args) {
         int[] values = {0, 1, 2, 3, 4, 5, 6,}; 
         
@@ -9,6 +28,11 @@ public class ReverseArray {
             values[i] = values[values.length - 1 -i]; 
             values[values.length - 1 - i] = temp; 
         }
-        System.out.println(java.util.Arrays.toString(values));
+        System.out.println(Arrays.toString(values));
+
+        // use static method of class ReverseArray
+        int[] arrayValues = { 0, 1, 2, 3, 4, 5, 6}; 
+        int[] reversed = ReverseArray.reverse(arrayValues); 
+        System.out.println(Arrays.toString(reversed));
     }
 }
